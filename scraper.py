b32776b544f26ee0251721062d60205f1e65fef5
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-07-30 15:29:27 +0530

    added documentation

--- scraper.py
+++ scraper.py
@@ -12,8 +12,15 @@ headers = {
     }
 
 
-def generate_link_attachment(url):
+def create_link_attachment(url):
+    """Creates website header for on-site navigation links to make then clickable from console
 
+    Args:
+        url (String): Default url or url provided by user
+
+    Returns:
+        String: Returns created website header
+    """
     link_attachment = ''
     slash_count = 0
     for char in url:
@@ -22,14 +29,19 @@ def generate_link_attachment(url):
         if slash_count > 2:
             break
         link_attachment += char
+
     return link_attachment
 
 
 def process_request(url):
+    """Calls upon the given url and gets back content of website in pure html form
 
+    Args:
+        url (String): Default url or url provided by user
+    """
     req = requests.get(url, headers=headers)
     soup = BeautifulSoup(req.content, 'html5lib')
-    link_attachment = generate_link_attachment(url)
+    link_attachment = create_link_attachment(url)
 
     if req.status_code >= 404:
         print(req.status_code)
@@ -40,14 +52,17 @@ def process_request(url):
         if not link:
             continue
 
+        # Checking for navigation links
         if link[0:4] != 'http':
+            # Adding website header to navigation link to make it clickable from console
             link = link_attachment + link
 
         print(link)
 
 
 def main():
-
+    """Takes command line argument
+    """
     parser = argparse.ArgumentParser(
                                     description='Extracts links contained in a url'
                                     )
@@ -62,6 +77,7 @@ def main():
 
     arg = parser.parse_args()
 
+    # Checking if url is valid or not
     if arg.url[0:4] != 'http':
         print('Invalid url')
         return
