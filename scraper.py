786753bae302a5c0df5819607790698b11625200
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-07-30 09:56:03 +0530

    organized code into main function

--- scraper.py
+++ scraper.py
@@ -1,17 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-def make_it_link(url):
-    link_attachment = ''
-    slash_count = 0
-    for char in url:
-        if char == '/':
-            slash_count += 1
-        if slash_count > 2:
-            break
-        link_attachment += char
-    return link_attachment
-
 
 headers = {
     'Access-Control-Allow-Origin': '*',
@@ -23,18 +12,37 @@ headers = {
 
 url = "https://github.com/ankushduacodes"
 
-req = requests.get(url, headers=headers)
 
-soup = BeautifulSoup(req.content, 'html5lib')
+def generate_link_attachment(url):
+    link_attachment = ''
+    slash_count = 0
+    for char in url:
+        if char == '/':
+            slash_count += 1
+        if slash_count > 2:
+            break
+        link_attachment += char
+    return link_attachment
 
-link_attachment = make_it_link(url)
 
-if req.status_code == 200 or req.status_code == 300:
+def main():
+    req = requests.get(url, headers=headers)
+    soup = BeautifulSoup(req.content, 'html5lib')
+    link_attachment = make_it_link(url)
+
+    if req.status_code == 400 or req.status_code == 500:
+        return
+
     for a_tag in soup.findAll('a'):
         link = a_tag['href']
         if not link:
             continue
-        
+
         if link[0] != 'h':
             link = link_attachment + link
+
         print(link)
+
+
+if __name__ == "__main__":
+    main()
